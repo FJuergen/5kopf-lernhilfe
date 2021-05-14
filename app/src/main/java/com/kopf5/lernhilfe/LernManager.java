aa60e8121e9275b2160ff04d8c8b879fb6fa52b0
Florens Jürgen <kerusei95@gmail.com>
2021-05-14 13:17:23 +0200

    Finished Timer Implementation

--- app/src/main/java/com/kopf5/lernhilfe/LernManager.java
+++ app/src/main/java/com/kopf5/lernhilfe/LernManager.java
@@ -2,16 +2,41 @@ package com.kopf5.lernhilfe;
 
 import android.os.CountDownTimer;
 
+import static com.kopf5.lernhilfe.IsRunning.*;
+
+enum IsRunning{
+    RUNNING,
+    PAUSED,
+    STOPPED
+}
+
 public class LernManager {
 
     Lerntimer parent;
     CountDownTimer timer = null;
+    public IsRunning isRunning = RUNNING;
+
+    long pausedTimerTime = 0;
+
 
     public LernManager(Lerntimer parentActivity){
         parent = parentActivity;
     }
+
     public void stopTimer(){
+        timer.cancel();
         timer = null;
+        isRunning = STOPPED;
+    }
+
+    public void pauseTimer(){
+        System.out.println("paused Timer");
+        stopTimer();
+        isRunning = PAUSED;
+    }
+
+    public void resumeTimer(){
+        startTimer(pausedTimerTime);
     }
 
     public void startTimer(long length){
@@ -20,7 +45,8 @@ public class LernManager {
 
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    parent.updateTimerText(millisUntilFinished + " ");
+                    parent.updateTimerText(millisUntilFinished/60000 + " : " +  String.format("%02d",(millisUntilFinished/1000)%60));
+                    pausedTimerTime = millisUntilFinished;
                 }
 
                 @Override
@@ -28,6 +54,7 @@ public class LernManager {
                     timer = null;
                 }
             }.start();
+            isRunning = RUNNING;
         }
     }
 }
