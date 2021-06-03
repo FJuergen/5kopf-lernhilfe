cc5ffe51626fc5ae92b5f3afeeba8f43dadd78d6
Florens Jürgen <kerusei95@gmail.com>
2021-06-03 22:00:56 +0200

    lerntimer zu fragment gemacht, toolbar bleibt vorhanden

--- app/src/main/java/com/kopf5/lernhilfe/Lerntimer.java
+++ app/src/main/java/com/kopf5/lernhilfe/Lerntimer.java
@@ -1,13 +1,23 @@
 package com.kopf5.lernhilfe;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,37 +26,52 @@ import java.util.TimerTask;
  * Einstellungen und Interface für den Lerntimer
  */
 
-public class Lerntimer extends AppCompatActivity {
+public class Lerntimer extends Fragment {
+
+    private static final String TAG = "Lerntimer";
 
     TextView clock;
     TextView level;
     LernManager manager = new LernManager(this);
     NumberPicker picker;
     AlertDialog dialog;
+    Button newTimer;
+    Button clockPause;
+    Button clockResume;
+    public BottomNavigationView bottomNavigationView;
+
+    public Lerntimer(){
+        super(R.layout.activity_lerntimer);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = inflater.inflate(R.layout.activity_lerntimer,container, false);
+        newTimer = (Button) view.findViewById(R.id.new_timer);
+        newTimer.setOnClickListener(this::newTimer);
+        clockPause = (Button) view.findViewById(R.id.clock_pause);
+        clockPause.setOnClickListener(this::pauseTimer);
+        clockResume = (Button) view.findViewById(R.id.clock_resume);
+        clockResume.setOnClickListener(this::resumeTimer);
+        clock = view.findViewById(R.id.clock);
+        level = view.findViewById(R.id.level);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        AlertDialog.Builder builder = new AlertDialog.Builder(view
+                .getContext());
         builder.setTitle(R.string.dialog_title);
         builder.setView(getLayoutInflater().inflate(R.layout.clock_dialog,null))
-        .setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startTimer(null);
-            }
-        })
-        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
+                .setPositiveButton(R.string.start, (dialog, which) -> startTimer(null))
+                .setNegativeButton(R.string.cancel, (dialog, which) -> dialog.cancel());
         dialog = builder.create();
-        setContentView(R.layout.activity_lerntimer);
-        clock = findViewById(R.id.clock);
-        level = findViewById(R.id.level);
-
+//
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -54,14 +79,16 @@ public class Lerntimer extends AppCompatActivity {
             }
         },0,100);
 
+
     }
 
     public void reset(){
-        findViewById(R.id.clock_pause).setVisibility(View.INVISIBLE);
-        findViewById(R.id.clock_resume).setVisibility(View.INVISIBLE);
+        clockPause.setVisibility(View.INVISIBLE);
+        clockResume.setVisibility(View.INVISIBLE);
     }
 
     public void newTimer(View view){
+
         dialog.show();
         if(picker==null) {
             picker = dialog.findViewById(R.id.clockPicker);
@@ -77,20 +104,20 @@ public class Lerntimer extends AppCompatActivity {
     public void startTimer(View view) {
         manager.startTimer(picker.getValue()*60000);
         dialog.hide();
-        findViewById(R.id.clock_pause).setVisibility(View.VISIBLE);
+        clockPause.setVisibility(View.VISIBLE);
     }
 
     public void pauseTimer(View view){
         manager.pauseTimer();
-        findViewById(R.id.clock_pause).setVisibility(View.INVISIBLE);
-        findViewById(R.id.clock_resume).setVisibility(View.VISIBLE);
+        clockPause.setVisibility(View.INVISIBLE);
+        clockResume.setVisibility(View.VISIBLE);
 
     }
 
     public void resumeTimer(View view){
         manager.resumeTimer();
-        findViewById(R.id.clock_resume).setVisibility(View.INVISIBLE);
-        findViewById(R.id.clock_pause).setVisibility(View.VISIBLE);
+        clockResume.setVisibility(View.INVISIBLE);
+        clockPause.setVisibility(View.VISIBLE);
 
     }
 }
