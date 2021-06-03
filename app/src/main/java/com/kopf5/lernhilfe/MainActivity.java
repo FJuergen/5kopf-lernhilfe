34b0be806b13d10d364606103cd5f38827693dbc
Florens Jürgen <kerusei95@gmail.com>
2021-06-03 22:00:56 +0200

    lerntimer zu fragment gemacht, toolbar bleibt vorhanden

--- app/src/main/java/com/kopf5/lernhilfe/MainActivity.java
+++ app/src/main/java/com/kopf5/lernhilfe/MainActivity.java
@@ -1,24 +1,55 @@
 package com.kopf5.lernhilfe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /*
  * Startbildschirm
  * Darstellung des Avatars, der XP und der aktuellen Lernziele
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    public MainActivity(){
+        super(R.layout.activity_main);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Intent test = new Intent(this,Lerntimer.class);
-        //startActivity(test);
-    }
 
-    // toolbar.xml steht (res/layout/toolbar.xml) - ist aber noch ohne Eventlistener.
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                item -> {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.navigation_timer:
+                            getSupportFragmentManager().beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .add(R.id.fragment_container,Lerntimer.class,null)
+                                    .commit();
+                            break;
+                        case R.id.navigation_happy:
+                            System.out.println("happy");
+                            break;
+                        case R.id.navigation_shop:
+                            System.out.println("shop");
+                            break;
+                        case R.id.navigation_clothes:
+                            System.out.println("clothes");
+                            break;
+                    }
+                    return true;
+                }
+        );
+    }
 }
