ee6f46ee968aae28845302ae4ebb63593feed684
Nicholas-Pfo <wertqwert123Nitch0>
2021-06-08 11:24:08 +0200

    Revert "wechseln der preview in der garderobe"
    This reverts commit 0e507477

--- app/src/main/java/com/kopf5/lernhilfe/AvatarSettings.java
+++ app/src/main/java/com/kopf5/lernhilfe/AvatarSettings.java
@@ -2,12 +2,7 @@ package com.kopf5.lernhilfe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 
 /*
  * Customization für den Avatar
@@ -15,29 +10,9 @@ import android.widget.ImageView;
 
 public class AvatarSettings extends AppCompatActivity {
 
-    ImageButton ausgewaehlterSkin;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avatar_settings);
-
     }
-
-    /*
-        Methode der ImageButtons:
-        setzt die Priview auf das Drawable des Buttons. alter Button wird enabled und neuer disabled.
-     */
-    public void setPreview(View v){
-        ausgewaehlterSkin = (ImageButton)v;
-        Drawable skin = ausgewaehlterSkin.getDrawable();
-        ((ImageView)findViewById(R.id.previewSkin)).setImageDrawable(skin);
-    }
-
-    //sendet den ausgewaehlten Skin an den Mainscreen
-    /*public void sendImage(Drawable skin){
-        Intent i = new Intent(this,RecieveImage.class);
-        String imageUri = "drawable://" + skin;
-        i.putExtra("skin",skin.toString());
-    }*/
 }
