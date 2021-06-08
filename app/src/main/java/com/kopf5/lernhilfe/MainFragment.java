18a95e15ac2c1d689fb5c3f07c65b5b4013e7126
Florens Jürgen <kerusei95@gmail.com>
2021-06-08 11:27:08 +0200

    pausen initial setup

--- app/src/main/java/com/kopf5/lernhilfe/MainFragment.java
+++ app/src/main/java/com/kopf5/lernhilfe/MainFragment.java
@@ -1,21 +1,17 @@
 package com.kopf5.lernhilfe;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/*
- * Shop interface um Avatar Optionen zu kaufen
- */
+public class MainFragment extends Fragment {
 
-public class AvatarShop extends Fragment {
-
-    public AvatarShop() {
-        super(R.layout.activity_avatar_shop);
+    public MainFragment() {
+        super(R.layout.fragment_main);
     }
 
     @Override
@@ -27,6 +23,6 @@ public class AvatarShop extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_avatar_shop, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
