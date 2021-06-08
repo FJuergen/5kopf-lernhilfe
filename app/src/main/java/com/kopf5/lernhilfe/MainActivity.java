cf15847e7ea6e6b388af8367ae7294f3682d2f27
Nicholas-Pfo <wertqwert123Nitch0>
2021-06-08 13:21:12 +0200

    anzeige des skins beim mainscreen

--- app/src/main/java/com/kopf5/lernhilfe/MainActivity.java
+++ app/src/main/java/com/kopf5/lernhilfe/MainActivity.java
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
--- app/src/main/java/com/kopf5/lernhilfe/MainFragment.java
+++ app/src/main/java/com/kopf5/lernhilfe/MainFragment.java
@@ -1,14 +1,27 @@
 package com.kopf5.lernhilfe;
 
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MainFragment extends Fragment {
+
+    private SharedPreferences mySP;
+    private ImageView img;
 
     public MainFragment() {
         super(R.layout.fragment_main);
@@ -23,6 +36,37 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        img = view.findViewById(R.id.imageView);
+        return view;
+    }
+
+
+
+    /*
+     * wird zur Activity navigiert, wird der Skin geladen.
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mySP = getActivity().getSharedPreferences("UserInfo",0);
+        loadImageFromStorage(mySP.getString("selectedSkinPath",""));
+    }
+
+
+    /*
+     * laedt Bild vom angegebenen Path
+     * setzt Image auf Standard Skin, falls File nicht gefunden
+     */
+    private void loadImageFromStorage(String path) {
+        try {
+            File f = new File(path, "chibiAvatar.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            img.setImageBitmap(b);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            //falls Datei nicht vorhanden, setzte den skin auf den Skin1
+            img.setImageDrawable(getResources().getDrawable(R.drawable.skin1));
+        }
     }
 }
